f9e3b9578374dc9ed08064bc6376405a715fe189
Saurav <ssanjay@palantir.com>
2024-10-15 18:18:26 -0400

    add to package.json

--- packages/platform-sdk-generator/src/addPackagesToPackageJson.ts
+++ packages/platform-sdk-generator/src/addPackagesToPackageJson.ts
@@ -26,6 +26,7 @@ export async function addPackagesToPackageJson(
     packageJson[section],
     Object.fromEntries(packagesToAdd.map(a => [a, "workspace:*"])),
   );
+  packageJson.dependencies["@osdk/shared.client"] = "1.0.1";
   packageJson.dependencies["@osdk/shared.client2"] = "1.0.0";
   packageJson.dependencies["@osdk/shared.net.platformapi"] = "~0.3.2";
   await fs.writeFile(
